986e387ac53934b67a8f466e13ccb20b608f939e
Geoff Lewis <glewisaz@gmail.com>
2026-04-22 18:24:07 -0700

    UPdated logging as commissioner

--- src/app/api/yahoo/debug/route.ts
+++ src/app/api/yahoo/debug/route.ts
@@ -2,6 +2,6 @@ import { yahooFetch } from '@/lib/yahoo/client'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const data = await yahooFetch('/users;use_login=1/games;game_keys=nfl/leagues')
+  const data = await yahooFetch('/users;use_login=1/games/leagues')
   return NextResponse.json(data)
 }
